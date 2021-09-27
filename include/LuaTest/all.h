2f674ffb31c0eae06436f80e5764e1a538e104b3
c53wei <c53wei@uwaterloo.ca>
2021-09-27 13:36:44 -0400

    Somehow lua add on is built. saving before things break

---
+++ include/LuaTest/all.h
@@ -0,0 +1,10 @@
+#ifndef BIORBD_LUA_ALL_H
+#define BIORBD_LUA_ALL_H
+
+#include "LuaTest/luamodel.h"
+#include "LuaTest/luastructs.h"
+#include "LuaTest/luatables.h"
+#include "LuaTest/luatypes.h"
+
+#endif // BIORBD_LUA_ALL_H
+
