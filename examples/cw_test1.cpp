89326717457d21e8aa720f28150412e2a999414b
c53wei <c53wei@uwaterloo.ca>
2021-10-27 11:56:13 -0400

    Namespace and include clean up

--- examples/cw_test1.cpp
+++ examples/cw_test1.cpp
@@ -1,52 +1,21 @@
 #include "biorbd.h"
-
-#include "LuaTest/luamodel.h"
-#include "LuaTest/luatables.h"
-#include "LuaTest/luatypes.h"
-extern "C"
-{
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
-}
-
-#include <rbdl/rbdl.h>
-#include "rbdl/rbdl_errors.h"
-
-#include "Utils/Path.h"
-#include "Utils/String.h"
-#include "Utils/Vector3d.h"
-
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <stdlib.h>
-#include <cstring>
-#include <iomanip>
-#include <map>
-#include <filesystem>
-
 
 using namespace std;
-using namespace RigidBodyDynamics;
-using namespace RigidBodyDynamics::Addons;
-using namespace RigidBodyDynamics::Math;
 using namespace BIORBD_NAMESPACE;
 
 
 int main()
 
 {
-    BiorbdEigen3::Model model("sagar_ior.lua");
+    Model model("sagar_ior.lua");
 
     cout << model.nbSegment() << endl; // 16
     cout << model.nbMarkers() << endl; // 49
     
     Writer::writeModel(model, "test_ior.bioMod");
     
-    BiorbdEigen3::Model model2("test_ior.bioMod");
+    Model model2("test_ior.bioMod");
 
     cout << model2.nbSegment() << endl; // 16
     cout << model2.nbMarkers() << endl; // 49
